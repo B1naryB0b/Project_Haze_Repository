64955e6f77e3848bb3058c3c870cae34a64f5c9f
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2018-06-17 00:03:18 +0100

    Initialising Repository

---
+++ Project_Haze-Game-Development/Assets/Prototype/Scripts/NPCBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NPCBehavior : MonoBehaviour {
+
+    NavMeshAgent agent;
+    GameObject destination;
+    GameObject player;
+
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+        {
+            Debug.LogWarning("No player is in scene");
+        }
+        agent = GetComponent<NavMeshAgent>();
+        GoTo();
+	}
+
+    void GoTo()
+    {
+        if (player)
+        {
+            agent.SetDestination(player.transform.position);
+            Debug.Log("NPC moving to destination: " + player.transform.position);
+        } else
+        {
+            Debug.LogWarning("NPC attempting to move to null navPoint");
+        }
+    }
+}
